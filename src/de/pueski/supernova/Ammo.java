eaf058a1b7d6b4bc2b729077fe1ab67b1f77352f
Matthias Püski <m.pueski@googlemail.com>
2015-03-31 17:34:22 +0200

    added ammo upgrade

---
+++ src/de/pueski/supernova/Ammo.java
@@ -0,0 +1,68 @@
+package de.pueski.supernova;
+
+import java.io.InputStream;
+
+import org.lwjgl.opengl.GL11;
+
+import de.pueski.supernova.tools.TextureUtil;
+
+public class Ammo extends Entity {
+
+	private static final float speed = 2.0f;
+	
+	private int texId;
+	
+	private long index; 
+		
+	public Ammo(float xLoc, float yLoc, int direction) {
+		this.xLoc = xLoc;
+		this.yLoc = yLoc;
+		this.index = System.currentTimeMillis();
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("ammo.png");		
+		this.texId = TextureUtil.loadTexture(is);
+		this.size = 32.0f;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (obj == this)
+			return true;
+		
+		if (obj.getClass() == getClass()) {
+			Ammo other = (Ammo)obj;
+			return (index == other.index);
+		}
+		
+		return false;
+	}
+	
+	public void fly() {
+		yLoc -= speed;
+	}
+	
+	public void draw() {
+		GL11.glColor3f(1.0f,1.0f,1.0f);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glPushMatrix();
+		GL11.glTranslatef(xLoc, yLoc, 0);	
+		GL11.glRotatef(90.0f, 0.0f,0.0f,1.0f);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texId);
+		GL11.glBegin(GL11.GL_POLYGON);
+		GL11.glVertex2f(-size, -size);
+		GL11.glTexCoord2f(0f, 0f);
+		GL11.glVertex2f(size, -size);
+		GL11.glTexCoord2f(1f, 0f);
+		GL11.glVertex2f(size, size);
+		GL11.glTexCoord2f(1f, 1f);
+		GL11.glVertex2f(-size, size);
+		GL11.glTexCoord2f(0f, 1f);
+		GL11.glEnd();
+		GL11.glPopMatrix();
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+	}
+
+	
+}
---
+++ src/de/pueski/supernova/Entity.java
@@ -0,0 +1,79 @@
+package de.pueski.supernova;
+
+public abstract class Entity implements IDrawable {
+
+	protected float xLoc;
+	protected float yLoc;
+	
+	protected int energy;
+	
+	protected float size;
+	private boolean visible = true;
+
+	/**
+	 * @return the xLoc
+	 */
+	public float getXLoc() {
+		return xLoc;
+	}
+
+	/**
+	 * @param xLoc the xLoc to set
+	 */
+	public void setXLoc(float xLoc) {
+		this.xLoc = xLoc;
+	}
+
+	/**
+	 * @return the yLoc
+	 */
+	public float getYLoc() {
+		return yLoc;
+	}
+
+	/**
+	 * @param yLoc the yLoc to set
+	 */
+	public void setYLoc(float yLoc) {
+		this.yLoc = yLoc;
+	}
+	
+	/**
+	 * @return the energy
+	 */
+	public int getEnergy() {
+		return energy;
+	}
+
+	/**
+	 * @param energy the energy to set
+	 */
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
+
+	public float getSize() {
+		return size;
+	}
+
+	public void setSize(float size) {
+		this.size = size;
+	}	
+	
+	public boolean isVisible() {
+		return visible ;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	
+	
+	public boolean collides(Entity entity) {
+		float x = entity.getXLoc();
+		float y = entity.getYLoc();
+		return (x >= xLoc - size && x <= xLoc + size && y >= yLoc - size && y <= yLoc + size);
+	}
+	
+	public abstract void fly();
+}
